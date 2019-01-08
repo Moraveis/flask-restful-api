e6dc541b556e3e263d6cbb1b0e604c5d6bcc9fe2
Moraveis <joao@codebit.com.br>
2019-01-08 08:43:22 -0200

    error handler

--- app_annotation.py
+++ app_annotation.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 
 app = Flask(__name__)
 
@@ -19,7 +19,24 @@ tasks = [
 
 @app.route("/api/v1/tasks", methods=['GET'])
 def get():
+    if len(task) == 0:
+        abort(404)
+    
     return jsonify({"task": tasks})
+
+@app.route('/api/v1/tasks/<int:task_id>', methods=['GET'])
+def get_by_id(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    
+    if len(task) == 0:
+        not_found(404)
+    
+    return jsonify({'task': task[0]})
+
+## error handling 
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
     app.run(debug=True)
